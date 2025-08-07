ec169c32297ec879b6884290eab1a581831a7c9d
shubhamkasar210 <shubhamkasar210@gmail.com>
2025-08-08 00:57:19 +0530

    update api completed

--- src/app.js
+++ src/app.js
@@ -54,6 +54,21 @@ app.delete("/user", async(req, res) => {
 });
 
 
+// Update data of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate({_id:userId}, data);
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+
+
+
 
 connectDB()
   .then(() => {
