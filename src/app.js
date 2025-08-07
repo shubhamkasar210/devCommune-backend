1d5ae1bf4d5c37091ca2511e747aa563e70627c5
shubhamkasar210 <shubhamkasar210@gmail.com>
2025-08-07 19:42:20 +0530

    making signup api dynamic

--- src/app.js
+++ src/app.js
@@ -3,13 +3,11 @@ const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "john",
-    lastName: "tendulkar",
-    emailId: "john@gmail.com",
-    password: "john@123",
-  });
+  // creating a new instance of a User model
+  const user = new User(req.body);
 
   try {
     await user.save();
