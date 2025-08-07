96e4477dbcf2036756562d3477a78908d172a0ae
shubhamkasar210 <shubhamkasar210@gmail.com>
2025-08-07 20:44:01 +0530

    delete api by id

--- src/app.js
+++ src/app.js
@@ -17,6 +17,44 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(400).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+// Feed API  - GET /feed -  get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.delete("/user", async(req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.status(400).send("something went wrong"); 
+  }
+});
+
+
+
 connectDB()
   .then(() => {
     console.log("Database connection established..");
