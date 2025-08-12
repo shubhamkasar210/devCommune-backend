a01cf34857818b3e19a36584598644c16fa6c50b
shubhamkasar210 <shubhamkasar210@gmail.com>
2025-08-12 08:46:49 +0530

    password encryption

--- src/app.js
+++ src/app.js
@@ -2,18 +2,52 @@ const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post("/signup", async (req, res) => {
-  // creating a new instance of a User model
-  const user = new User(req.body);
-
   try {
+    // validation of data
+    validateSignUpData(req);
+
+    const { firstName, lastName, emailId, password } = req.body;
+    // encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    // creating a new instance of a User model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
     await user.save();
     res.send("User Added Successfully!");
   } catch (err) {
-    res.status(400).send("Error saving the user:" + err.message);
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      res.send("Login Successful");
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
   }
 });
 
@@ -63,11 +97,11 @@ app.patch("/user/:userId", async (req, res) => {
       ALLOWED_UPDATES.includes(k)
     );
 
-    if(!isUpdateAllowed) {
+    if (!isUpdateAllowed) {
       throw new Error("Update not allowed");
     }
 
-    if(data?.skills.length > 10) {
+    if (data?.skills.length > 10) {
       throw new Error("Skills cannot be more than 10");
     }
 
