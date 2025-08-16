2083f52a13f4858f66fc6a4358d4b0d3884f9e6b
shubhamkasar210 <shubhamkasar210@gmail.com>
2025-08-16 12:42:35 +0530

    improved auth.js response

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -49,7 +49,7 @@ authRouter.post("/login", async (req, res) => {
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
       });
-      res.send(user);
+      res.json({ message: "Login Successfull!", data: user });
     } else {
       throw new Error("Invalid credentials");
     }
