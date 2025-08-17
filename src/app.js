44cc6850a7ff1050916f0efc87ba19fe3975d0c6
shubhamkasar210 <shubhamkasar210@gmail.com>
2025-08-17 18:16:00 +0530

    cors origin updated

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 app.use(
   cors({
     origin: "http://localhost:5173",
-    origin: "https://devcommune-frontend-sk.vercel.app/",
+    origin: "https://devcommune-frontend-sk.vercel.app",
     credentials: true,
   })
 );
